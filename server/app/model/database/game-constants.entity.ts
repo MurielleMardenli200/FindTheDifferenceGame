912d7f1109b68b34dd8e4eaeed0e97875d4319d0
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2024-01-24 20:12:07 -0500

    Removed constructor for GameConstants entity

--- server/app/model/database/game-constants.entity.ts
+++ server/app/model/database/game-constants.entity.ts
@@ -1,30 +1,18 @@
 import { GameConstants as GameConstantsInterface } from '@common/game-constants';
 import { INIT, PENALTY, WIN } from '@common/game-default.constants';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { v4 as uuid } from 'uuid';
 @Entity()
 export class GameConstants implements GameConstantsInterface {
     @PrimaryGeneratedColumn('uuid')
     // FIXME: Exclude from api
-    id: string;
+    id!: string;
 
     @Column({ nullable: false, default: INIT })
-    initialTime: number;
+    initialTime!: number;
 
     @Column({ nullable: false, default: PENALTY })
-    hintPenalty: number;
+    hintPenalty!: number;
 
     @Column({ nullable: false, default: WIN })
-    differenceFoundBonus: number;
-
-    // TODO: see if we can remove constructor
-    constructor(gameConstants: GameConstants = {
-        initialTime: INIT, hintPenalty: PENALTY, differenceFoundBonus: WIN,
-        id: ''
-    }) {
-        this.id = uuid();
-        this.initialTime = gameConstants.initialTime;
-        this.hintPenalty = gameConstants.hintPenalty;
-        this.differenceFoundBonus = gameConstants.differenceFoundBonus;
-    }
+    differenceFoundBonus!: number;
 }
