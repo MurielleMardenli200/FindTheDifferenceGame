fbc4ce002ddcbcc9f6b52fee2b49392740c9a1ea
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2024-01-28 16:18:03 -0500

    Created bigint transformer to deserealize it as number instead of string

--- server/app/model/database/game-history.entity.ts
+++ server/app/model/database/game-history.entity.ts
@@ -1,17 +1,17 @@
 import { GameMode } from '@common/game-mode';
 import { History as HistoryInterface } from '@common/model/history';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { bigIntTransformer } from './transformers/bigint.transformer';
 
-//
 @Entity()
 export class History implements HistoryInterface {
     @PrimaryGeneratedColumn('uuid')
     id!: string;
 
-    @Column('bigint')
+    @Column('bigint', { transformer: bigIntTransformer })
     gameStart!: number;
 
-    @Column('bigint')
+    @Column()
     gameTime!: number;
 
     @Column()
