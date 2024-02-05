83571b14981be4caad6ed65afb6f49ebdf34cb5e
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2024-02-04 20:27:59 -0500

    Added message bar, created MessageBubble widget

--- client_leger/lib/pages/chat.dart
+++ client_leger/lib/pages/chat.dart
@@ -1,4 +1,6 @@
 import 'package:client_leger/services/chat_service.dart';
+import 'package:client_leger/widgets/message_bubble.dart';
+import 'package:client_leger/widgets/message_input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,8 +14,15 @@ class ChatPage extends StatelessWidget {
     return ListView(
       children: [
         for (var message in chatService.messages)
-          Text(
-              '${message.time.toLocal()} ${message.author}: ${message.content}'),
+          MessageBubble(message: message),
+        // FIXME: Put the message bar at the bottom
+        // Based on https://stackoverflow.com/questions/45746636/trying-to-bottom-center-an-item-in-a-column-but-it-keeps-left-aligning
+        const Align(
+          alignment: FractionalOffset.bottomCenter,
+          child: Padding(
+              padding: EdgeInsets.only(bottom: 12.0),
+              child: MessageInputField()),
+        ),
       ],
     );
   }
---
+++ client_leger/lib/widgets/message_bubble.dart
@@ -0,0 +1,27 @@
+import 'package:client_leger/interfaces/chat_message.dart';
+import 'package:client_leger/services/chat_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({
+    super.key,
+    required this.message,
+  });
+
+  final ChatMessage message;
+
+  @override
+  Widget build(BuildContext context) {
+    AlignmentGeometry alignment =
+        context.watch<ChatService>().isMessageSentByUser(message)
+            ? Alignment.centerRight
+            : Alignment.centerLeft;
+
+    return Align(
+      alignment: alignment,
+      child: Text(
+          '${message.time.hour}:${message.time.minute}:${message.time.second} ${message.author}: ${message.content}'),
+    );
+  }
+}
